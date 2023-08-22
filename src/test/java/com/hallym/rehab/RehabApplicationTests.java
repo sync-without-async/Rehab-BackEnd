afad5d9dbefcc277d8a45902226853ddd364b715
sirius506775 <sirius506775@gmail.com>
2023-08-23 06:56:49 +0900

    fix : fix bean overriding error
    @EnableJpaAuditing 어노테이션이 Applcation 메소드 뿐만이 아니라 다른 클래스에도 추가가 되어있던 탓에 build 시 bean override가 발생했습니다.
    
    Spring boot 2.1 부터 overriding 옵션이 false로 default 설정되어있기 때문에bean 생성하는 과정에서 에러가 발생한 것이 build 실패의 원인이었습니다.

--- src/test/java/com/hallym/rehab/RehabApplicationTests.java
+++ src/test/java/com/hallym/rehab/RehabApplicationTests.java
@@ -2,10 +2,8 @@ package com.hallym.rehab;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootTest
-@EnableJpaAuditing //AuditingEntityListener 활성화
 class RehabApplicationTests {
 
 	@Test
