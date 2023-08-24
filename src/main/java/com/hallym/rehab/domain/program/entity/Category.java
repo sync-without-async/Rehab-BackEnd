c4691117432100f950be121eff1529fc409451f7
sirius506775 <sirius506775@gmail.com>
2023-08-24 17:28:42 +0900

    feat : Category와 postion enum 클래스 생성

--- src/main/java/com/hallym/rehab/domain/program/entity/Category.java
+++ src/main/java/com/hallym/rehab/domain/program/entity/Category.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 
 public enum Category {
 
-    UPPER,//상지
-    LOWER; //하지
+    SHOULDERS,//어깨
+    ARMS, //팔
+    KNEES, //무릎
+    THIGHS; //허벅지
 
     @JsonCreator
     public static Category from(String s){
