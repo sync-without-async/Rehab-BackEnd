265bf32559892834c97dad6b94f6a378b83bbcc9
sirius506775 <sirius506775@gmail.com>
2023-08-24 17:32:04 +0900

    refactor : program, video 엔티티 및 dto 수정

--- src/main/java/com/hallym/rehab/domain/program/entity/Program.java
+++ src/main/java/com/hallym/rehab/domain/program/entity/Program.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.hallym.rehab.domain.user.entity.Member;
 import com.hallym.rehab.global.baseEntity.BaseTimeEntity;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -25,17 +26,41 @@ public class Program extends BaseTimeEntity {
     @Column(name = "title")
     private String programTitle;
 
-    @Enumerated(EnumType.STRING)
-    private Category Category;
-
     private String description;
 
-    @ManyToOne
-    private Member member;
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @Enumerated(EnumType.STRING)
+    private Position position;
 
     @Builder.Default
     @OneToMany(mappedBy = "program", fetch = FetchType.LAZY)
     private Set<ProgramVideo> programVideo = new HashSet<>();
 
+    @ManyToOne
+    private Member member;
 
+    @ColumnDefault("false") //삭제 여부
+    private boolean is_deleted;
+
+    public void modifyProgram(String programTitle, String description, Category category, Position position){
+        this.programTitle = programTitle;
+        this.description = description;
+        this.category = category;
+        this.position = position;
+    }
+
+    public void addProgramVideo(String GuideVideoPath, String JsonPath){} //비디오 파일 추가 메소드
+
+    public void clearProgramVideo(){ //비디오 파일 변경 시 사용할 메소드
+
+        programVideo.forEach(programVideo -> programVideo.changeProgram(null));
+
+        this.programVideo.clear();
+    }
+
+    public void setIs_deleted(Boolean is_deleted){ //soft delete
+        this.is_deleted = is_deleted;
+    }
 }
