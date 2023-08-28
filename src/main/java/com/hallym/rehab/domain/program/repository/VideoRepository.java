c8a6d4c2e780281944f9d05a65e1152f0d887439
mythpoy <okmlnsunok@gmail.com>
2023-08-28 23:32:45 +0900

    feat : findByPno 추가

--- src/main/java/com/hallym/rehab/domain/program/repository/VideoRepository.java
+++ src/main/java/com/hallym/rehab/domain/program/repository/VideoRepository.java
@@ -3,10 +3,15 @@ package com.hallym.rehab.domain.program.repository;
 import com.hallym.rehab.domain.program.entity.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface VideoRepository extends JpaRepository<Video, Long> {
 
     @Query("select pv from Program p join p.video pv where p.pno = :pno and pv.ord = :ord")
-    Optional<Video> findByPno(Long pno, Long ord);
+    Optional<Video> findByPnoAndOrd(Long pno, Long ord);
+
+    @Query("select pv from Program p join p.video pv where p.pno = :pno")
+    List<Video> findByPno(Long pno);
 }
