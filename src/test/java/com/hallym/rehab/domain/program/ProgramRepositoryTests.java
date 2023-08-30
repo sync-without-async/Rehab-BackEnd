a2152d0beadd4a935305a8dba16eb96b99521888
sirius506775 <sirius506775@gmail.com>
2023-08-30 17:39:13 +0900

    test : add testInsertDummyTest

--- src/test/java/com/hallym/rehab/domain/program/ProgramRepositoryTests.java
+++ src/test/java/com/hallym/rehab/domain/program/ProgramRepositoryTests.java
@@ -4,37 +4,50 @@ import com.hallym.rehab.domain.program.entity.Category;
 import com.hallym.rehab.domain.program.entity.Position;
 import com.hallym.rehab.domain.program.entity.Program;
 import com.hallym.rehab.domain.program.repository.ProgramRepository;
-import com.hallym.rehab.domain.program.repository.VideoRepository;
-import com.hallym.rehab.domain.user.entity.Member;
-import com.hallym.rehab.domain.user.entity.MemberRole;
-import com.hallym.rehab.domain.user.repository.MemberRepository;
+
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Collections;
-import java.util.Optional;
+import java.util.stream.IntStream;
 
 @SpringBootTest
 @Log4j2
-public class ProgramReposotoryTests {
+public class ProgramRepositoryTests {
 
     @Autowired
     private ProgramRepository programRepository;
 
     @Test
-    public void registerProgram() throws Exception {
+    public void registerProgramTest() throws Exception {
 
         Program program = Program.builder()
                 .programTitle("sample title")
                 .description("Program test")
-                .category(Category.ARMS)
-                .position(Position.SITTING)
+                .category(Category.SHOULDERS)
+                .position(Position.LYING)
                 .build();
 
         programRepository.save(program);
 
         log.info(program.toString());
+    }
+
+    @Test
+    public void testInsertDummyTest() throws Exception {
+
+        IntStream.rangeClosed(1, 9).forEach(i -> {
+
+            Program result = programRepository.save(Program.builder()
+                    .programTitle("sample title")
+                    .description("Program test")
+                    .category(Category.THIGHS)
+                    .position(Position.LYING)
+                    .build());
+
+            log.info(result.toString());
+        });
+
     }
 }
