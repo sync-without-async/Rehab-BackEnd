68af6f3a808401a3d2793d38cce2b8428ae857a1
sirius506775 <sirius506775@gmail.com>
2023-08-30 17:08:23 +0900

    refactor : @Query 사용 시, parameter에 Spring Data가 제공하는 @Param 추가

--- src/main/java/com/hallym/rehab/domain/program/repository/Video_MemberRepository.java
+++ src/main/java/com/hallym/rehab/domain/program/repository/Video_MemberRepository.java
@@ -3,10 +3,11 @@ package com.hallym.rehab.domain.program.repository;
 import com.hallym.rehab.domain.program.entity.Video_Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface Video_MemberRepository extends JpaRepository<Video_Member, Long> {
     @Query("select vm from Video_Member vm where vm.member.mid = :mid and vm.video.vno = :vno")
-    Optional<Video_Member> findByMemberAndVideo(String mid, Long vno);
+    Optional<Video_Member> findByMemberAndVideo(@Param("mid") String mid, @Param("vno") Long vno);
 }
