c27cb3f0d02ae3ce5a5f26b3181775db122a2eee
sirius506775 <sirius506775@gmail.com>
2023-09-03 15:32:35 +0900

    feat : @PreAuthorize 어노테이션을 이용한 엔드포인트 인증처리

--- src/main/java/com/hallym/rehab/domain/program/controller/ProgramAdminController.java
+++ src/main/java/com/hallym/rehab/domain/program/controller/ProgramAdminController.java
@@ -5,6 +5,8 @@ import com.hallym.rehab.domain.program.service.ProgramService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -17,23 +19,25 @@ public class ProgramAdminController {
 
     private final ProgramService programService;
 
-//    @PreAuthorize("authentication.principal.username == #passwordChangeDTO.mid or hasRole('ROLE_ADMIN')")
+    @PreAuthorize("authentication.principal.username == #programRequestDTO.mid or hasRole('ROLE_ADMIN')")
     @PostMapping("/create")
-    public Map<String, Long> createProgram(@ModelAttribute ProgramRequestDTO programRequestDTO) {
+    public Map<String, Long> createProgram(@RequestBody ProgramRequestDTO programRequestDTO) {
         Long result = programService.createProgram(programRequestDTO);
         return Map.of("Program number : ", result);
     }
 
+    @PreAuthorize("authentication.principal.username == #programRequestDTO.mid or hasRole('ROLE_ADMIN')")
     @PutMapping("/modify/{pno}")
     public ResponseEntity<String> modifyProgram(@PathVariable Long pno,
-                                                @ModelAttribute ProgramRequestDTO programRequestDTO) {
+                                                @RequestBody ProgramRequestDTO programRequestDTO) {
         String result = programService.modifyProgramOne(pno, programRequestDTO);
         return ResponseEntity.ok(result);
     }
 
+    @PreAuthorize("authentication.principal.username == #programRequestDTO.mid or hasRole('ROLE_ADMIN')")
     @DeleteMapping("/delete/{pno}")
-    public ResponseEntity<String> deleteProgram(@PathVariable Long pno) {
-        String result = programService.deleteProgramOne(pno);
+    public ResponseEntity<String> deleteProgram(@PathVariable Long pno, @RequestBody ProgramRequestDTO programRequestDTO) {
+        String result = programService.deleteProgramOne(pno, programRequestDTO);
         return ResponseEntity.ok(result);
     }
 
