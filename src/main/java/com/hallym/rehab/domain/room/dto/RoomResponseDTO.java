1a40de287eab458c31e00e5bb2d9f06905dc41b3
mythpoy <okmlnsunok@gmail.com>
2023-09-18 23:58:27 +0900

    test : Room req, res DTO 생성

---
+++ src/main/java/com/hallym/rehab/domain/room/dto/RoomResponseDTO.java
@@ -0,0 +1,19 @@
+package com.hallym.rehab.domain.room.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Builder
+@Data
+@AllArgsConstructor
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class RoomResponseDTO {
+    private UUID rno;
+    private String admin_id;
+    private String user_id;
+}
