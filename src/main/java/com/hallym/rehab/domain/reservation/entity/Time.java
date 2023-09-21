23dea23928528b0a5c555d69849e16ce01c093d8
mythpoy <okmlnsunok@gmail.com>
2023-09-21 19:45:02 +0900

    feat : time entity 생성

---
+++ src/main/java/com/hallym/rehab/domain/reservation/entity/Time.java
@@ -0,0 +1,38 @@
+package com.hallym.rehab.domain.reservation.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.hallym.rehab.domain.admin.entity.Admin;
+import com.hallym.rehab.global.baseEntity.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "reservation_time")
+@Entity
+public class Time {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long tno;
+
+    private LocalDate date;
+
+    @Column(name = "time_index")
+    private int index;
+
+    @JsonBackReference
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mid", nullable = false)
+    private Admin admin;
+
+    public void setAdmin(Admin admin) {
+        this.admin = admin;
+    }
+}
