5aa714118310d92afe9066a058c2ce2645ab4eb3
mythpoy <okmlnsunok@gmail.com>
2023-09-22 23:07:02 +0900

    feat : mid를 이용해 Reservation 찾는 query 추가

--- src/main/java/com/hallym/rehab/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/hallym/rehab/domain/reservation/repository/ReservationRepository.java
@@ -2,7 +2,14 @@ package com.hallym.rehab.domain.reservation.repository;
 
 import com.hallym.rehab.domain.reservation.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
+    @Query("SELECT r FROM Reservation r WHERE r.user.mid = :mid or r.admin.mid = :mid " +
+            "ORDER BY r.date ASC, r.index ASC")
+    List<Reservation> findByMid(@Param("mid") String mid);
 }
