d7a79aa02f72449f98423a2960ec77b39b67013f
sirius506775 <sirius506775@gmail.com>
2023-09-26 02:05:11 +0900

    feat : Implement EmailController (#10)
    REST Controller인 EmailController 클래스를 정의합니다. EmailService 인터페이스를 주입받아 이메일을 전송하는 sendMail() handler method를 구현합니다.

---
+++ src/main/java/com/hallym/rehab/domain/email/controller/EmailController.java
@@ -0,0 +1,26 @@
+package com.hallym.rehab.domain.email.controller;
+
+import com.hallym.rehab.domain.email.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/email")
+public class EmailController {
+
+    private final EmailService emailService;
+
+    @PostMapping("/send")
+    public String sendMail(@RequestParam(value = "file", required = false) MultipartFile[] file, String to, String[] cc, String subject, String body) {
+
+        return emailService.sendMail(file, to, cc, subject, body);
+    }
+
+}
