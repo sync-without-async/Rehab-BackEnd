b392319891761e30662aa1e0d47a1be81d4fcfe3
mythpoy <okmlnsunok@gmail.com>
2023-09-26 17:29:39 +0900

    test : Video 검색기능 테스트

--- src/test/java/com/hallym/rehab/domain/video/repository/VideoRepositoryTest.java
+++ src/test/java/com/hallym/rehab/domain/video/repository/VideoRepositoryTest.java
@@ -3,23 +3,26 @@ package com.hallym.rehab.domain.video.repository;
 import com.hallym.rehab.domain.admin.entity.Admin;
 import com.hallym.rehab.domain.admin.repository.AdminRepository;
 import com.hallym.rehab.domain.user.entity.MemberRole;
+import com.hallym.rehab.domain.video.dto.pagedto.VideoPageRequestDTO;
+import com.hallym.rehab.domain.video.dto.VideoResponseDTO;
 import com.hallym.rehab.domain.video.entity.Tag;
 import com.hallym.rehab.domain.video.entity.Video;
-import org.assertj.core.api.Assertions;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 import org.springframework.test.annotation.Rollback;
 
 import javax.transaction.Transactional;
 import java.util.Collections;
-import java.util.Optional;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Slf4j
 @Transactional
 class VideoRepositoryTest {
 
@@ -64,6 +67,51 @@ class VideoRepositoryTest {
                 .orElseThrow(() -> new RuntimeException("정확하지 않은 비디오 번호 입니다."));
 
         assertThat(saveVideo).isEqualTo(savedVideo);
+    }
+
+    @Test
+    @Rollback(value = false)
+    void videoSearch() {
+        // insert dummy video data
+        for (int i = 0; i < 20; i++) {
+            Video video = Video.builder()
+                    .admin(admin)
+                    .title("동작 제목" + i)
+                    .description("동작 설명" + i)
+                    .tag(Tag.ARM)
+                    .frame(300L + i)
+                    .playTime(18.5 + i)
+                    .videoURL("https://..." + i)
+                    .jsonURL("https://..." + i)
+                    .videoPath("video/.." + i)
+                    .jsonPath("json/.." + i)
+                    .build();
+            videoRepository.save(video);
+        }
+
+        // search condition
+        String title = "2"; // contain
+        Tag tag = Tag.ARM; // equal
+
+        VideoPageRequestDTO pageRequestDTO = VideoPageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .title(title)
+                .tag(tag)
+                .build();
+
+        Page<VideoResponseDTO> search = videoRepository.search(pageRequestDTO);
+        List<VideoResponseDTO> content = search.getContent();
+        log.info("total count == " + String.valueOf(content.size()));
+        content.forEach(videoResponseDTO -> {
+            log.info(String.valueOf(videoResponseDTO.getVno()));
+            log.info(videoResponseDTO.getTitle());
+            log.info(videoResponseDTO.getDescription());
+            log.info(String.valueOf(videoResponseDTO.getTag()));
+            log.info(String.valueOf(videoResponseDTO.getPlayTime()));
+            log.info(videoResponseDTO.getVideoURL());
+            log.info("========================================");
+        });
     }
 
 }
