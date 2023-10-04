391bc5d41e8f9d56da10e2d597270b43142af511
mythpoy <okmlnsunok@gmail.com>
2023-10-04 17:30:04 +0900

    feat : create VideoMetrics Entity

---
+++ src/main/java/com/hallym/rehab/domain/video/entity/VideoMetrics.java
@@ -0,0 +1,39 @@
+package com.hallym.rehab.domain.video.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.hallym.rehab.domain.program.entity.ProgramDetail;
+import com.hallym.rehab.domain.user.entity.Member;
+import com.hallym.rehab.global.baseEntity.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class VideoMetrics extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long vmno;
+
+    @JsonBackReference
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private Member user;
+
+    @OneToOne
+    @JoinColumn(name = "pdno", referencedColumnName = "pdno")
+    private ProgramDetail programDetail;
+
+    private double metrics;
+
+    public void updateMetrics(double metrics) {
+        this.metrics = metrics;
+    }
+}
