8d2d71046053e04b3e3d0a09b030b16b32274839
mythpoy <okmlnsunok@gmail.com>
2023-11-14 15:28:38 +0900

    refactor: RedirectConnector 삭제

--- src/main/java/com/hallym/rehab/global/config/SslConfig.java
+++ src/main/java/com/hallym/rehab/global/config/SslConfig.java
@@ -25,16 +25,6 @@ public class SslConfig {
                 context.addConstraint(securityConstraint);
             }
         };
-        tomcat.addAdditionalTomcatConnectors(redirectConnector());
         return tomcat;
-    }
-
-    private Connector redirectConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        connector.setPort(8080);
-        connector.setSecure(false);
-        connector.setRedirectPort(8443);
-        return connector;
     }
 }
