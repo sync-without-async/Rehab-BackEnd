9de5cf0036ca61df304def58f66641a46985732d
sirius506775 <sirius506775@gmail.com>
2023-11-16 06:19:36 +0900

    feature : Add @PreAuthorize fo Authentication to allow api (#25)

--- src/main/java/com/hallym/rehab/domain/chart/controller/ChartController.java
+++ src/main/java/com/hallym/rehab/domain/chart/controller/ChartController.java
@@ -29,11 +29,29 @@ public class ChartController {
         return chartOne;
     }
 
-    @PreAuthorize("authentication.principal.username == #chartRequestDTO.doctor_id")
+
+    @PreAuthorize("hasAuthority('ROLE_DOCTOR')")
     @PostMapping("/auth/register")
     public ResponseEntity<String> registerChart(@Valid @RequestBody ChartRequestDTO chartRequestDTO) {
 
             chartService.registerChartDetails(chartRequestDTO);
             return ResponseEntity.ok("차트 등록 완료");
     }
+
+    @PreAuthorize("hasAuthority('ROLE_DOCTOR')")
+    @PostMapping("/auth/delete")
+    public ResponseEntity<String> deleteChart(@PathVariable Long cno) {
+
+        chartService.deleteChartDetails(cno);
+        return ResponseEntity.ok("차트 삭제 완료");
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_DOCTOR') and authentication.principal.username == #doctor_id")
+    @GetMapping("/auth/list/{doctor_id}")
+    public PageResponseDTO<ChartResponseDTO> getChartList(PageRequestDTO pageRequestDTO, @PathVariable String doctor_id){
+
+        return chartService.getChartList(doctor_id, pageRequestDTO);
+    }
+
+
 }
