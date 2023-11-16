f7ad92f2dab2f5fe774dc19545154ac9fc25bd41
sirius506775 <sirius506775@gmail.com>
2023-11-16 12:55:20 +0900

    feature : Add PreAuthorize annotation to authenticate  ROLE_DOCTOR on registerRecord(#2)

--- src/main/java/com/hallym/rehab/domain/chart/controller/RecordController.java
+++ src/main/java/com/hallym/rehab/domain/chart/controller/RecordController.java
@@ -6,6 +6,7 @@ import com.hallym.rehab.global.pageDTO.PageRequestDTO;
 import com.hallym.rehab.global.pageDTO.PageResponseDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
